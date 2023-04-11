f7bfd6621b78e7284b618efe149b0f7765393ad9
duxphuc20703 <ducphuc20703@gmail.com>
2023-04-11 16:12:13 +0700

    dmm

--- src/bankpayment.php
+++ src/bankpayment.php
@@ -15,6 +15,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
 </head>
 <body>
+    
 <?php
 if(isset( $_POST['idpay'])){
     $id = $_POST['idpay'];
